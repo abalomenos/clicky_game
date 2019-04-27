a89ae6b486a820600b56d8b62707e892c85645c5
abalomenos <arisbalomenos@gmail.com>
2019-04-27 11:34:05 -0700

    Updates

---
+++ precache-manifest.ef617bcf7532ac0f1da6bf3033e5b4c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1d35926ae3e28420e6c6066f6e6700",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "fd06035880829806d2eb",
+    "url": "/react-clicky-game/static/css/main.302cedf1.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/react-clicky-game/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "fd06035880829806d2eb",
+    "url": "/react-clicky-game/static/js/main.0632262a.chunk.js"
+  },
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  }
+]);
